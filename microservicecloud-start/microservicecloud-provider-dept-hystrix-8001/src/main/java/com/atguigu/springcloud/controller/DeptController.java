c33707621b59ac66b1ce4a992a8fc832c147ff29
liuqiang <liuq@jccfc.com>
2019-07-09 18:34:46 +0800

    服务熔断

---
+++ microservicecloud-start/microservicecloud-provider-dept-hystrix-8001/src/main/java/com/atguigu/springcloud/controller/DeptController.java
@@ -0,0 +1,72 @@
+package com.atguigu.springcloud.controller;
+
+import java.util.List;
+
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.atguigu.springcloud.entities.Dept;
+import com.atguigu.springcloud.service.DeptService;
+
+@RestController
+public class DeptController
+{
+	@Autowired
+	private DeptService service;
+	@Autowired
+	private DiscoveryClient client;
+
+	@RequestMapping(value = "/dept/add", method = RequestMethod.POST)
+	public boolean add(@RequestBody Dept dept)
+	{
+		return service.add(dept);
+	}
+
+	@RequestMapping(value = "/dept/get/{id}", method = RequestMethod.GET)
+	@HystrixCommand(fallbackMethod = "processHystrix_Get")
+	public Dept get(@PathVariable("id") Long id)
+	{
+		if(service.get(id)==null){
+			throw  new RuntimeException("沒有对应的部门ID");
+		}
+		return service.get(id);
+	}
+
+	@RequestMapping(value = "/dept/list", method = RequestMethod.GET)
+	public List<Dept> list()
+	{
+		return service.list();
+	}
+
+
+
+	public Dept processHystrix_Get(@PathVariable("id") Long id){
+		return  new Dept().setDeptno(9999L).setDname("沒有对应的部门ID  in this ID，提供服务熔断 @HystrixCommand  ");
+	}
+
+	
+//	@Autowired
+//	private DiscoveryClient client;
+	//对于注册进eureka里面的微服务，可以通过服务发现来获得该服务的信息
+	@RequestMapping(value = "/dept/discovery", method = RequestMethod.GET)
+	public Object discovery()
+	{
+		List<String> list = client.getServices();
+		System.out.println("**********" + list);
+
+		List<ServiceInstance> srvList = client.getInstances("MICROSERVICECLOUD-DEPT");
+		for (ServiceInstance element : srvList) {
+			System.out.println(element.getServiceId() + "\t" + element.getHost() + "\t" + element.getPort() + "\t"
+					+ element.getUri());
+		}
+		return this.client;
+	}
+
+}
